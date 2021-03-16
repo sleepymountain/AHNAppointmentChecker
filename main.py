7a3f621694e3bafa1d2f0268828128c848fe5359
sleepymountain <65647838+sleepymountain@users.noreply.github.com>
2021-03-16 17:53:27 -0400

    v1.5b Update
    New in v1.5b
    - Better UI Formatting
    - Colorama! Output of console now colored.
    - 12 hour time, instead of 24 hour time.
    - Logging: All results are outputted into \log\log.txt (for easier analysis of appointment posting times)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from selenium.webdriver.remote.remote_connection import LOGGER
 from win10toast import ToastNotifier
 import sched, time
 from datetime import datetime
+from colorama import Fore
 
 toaster = ToastNotifier()
 s = sched.scheduler(time.time, time.sleep)
@@ -21,34 +22,39 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 def start():
 
-    print(f'AHN Appointment Checker v1.2b | by caverin/sleepymountain')
-    print(f'https://github.com/sleepymountain/AHNAppointmentChecker')
+    print(Fore.GREEN + 'AHN Appointment Checker v1.5b | by caverin/sleepymountain')
+    print(Fore.GREEN + 'https://github.com/sleepymountain/AHNAppointmentChecker')
     print(f'')
-    print(f'Running appointment checker..')
+    print(Fore.BLUE + '[i] Running appointment checker..')
     print(f'')
     LOGGER.setLevel(logging.WARNING)
 
 
 if __name__ == '__main__':
     start()
+    if not os.path.exists('log'):
+        os.makedirs('log')
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M %p', filename='log/log.txt', level=logging.INFO)
 
 
 def load_page():
     with webdriver.Chrome(options=chrome_options) as driver:
         now = datetime.now()
-        dt_string = now.strftime("%m/%d/%Y %H:%M")
-        print(f'Checking for new appointments..')
+        dt_string = now.strftime("%m/%d/%Y %I:%M %p")
+        print(Fore.BLUE + '[i] Checking for new appointments..')
         wait = WebDriverWait(driver, 10)
         driver.get("https://www.ahn.org/coronavirus/vaccine/schedule.html")
         if driver.find_element_by_xpath("/html/body/main/div[2]/div/section/div/div/div/div[1]/div/h1"):
-            print(dt_string, " | There are no appointments available")
+            print(Fore.RED + dt_string, " | There are no appointments available")
+            logging.info(' | There are no appointments available')
             print(f'')
         else:
-            print(dt_string, " | There might be appointments available!")
+            print(Fore.GREEN + "[!]", dt_string, " | There might be appointments available!")
+            logging.info(' | There might be appointments available!')
             print(f'')
             toaster.show_toast("AHN Appointment Checker", "There are appointments available!")
             os.startfile(url)
-        print(f'Waiting 5 minutes.')
+        print(Fore.BLUE + '[i] Waiting 5 minutes.')
         time.sleep(300)
 
 
