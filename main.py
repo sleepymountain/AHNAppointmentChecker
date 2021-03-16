940514d8bf35bf77b43ad1322cb90434772b7ecb
sleepymountain <65647838+sleepymountain@users.noreply.github.com>
2021-03-16 18:08:18 -0400

    v1.5b Update
    Removed Colorama from last v1.5b release, due to it causing issues when compiling to an executable.

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 def start():
 
-    print(Fore.GREEN + 'AHN Appointment Checker v1.5b | by caverin/sleepymountain')
-    print(Fore.GREEN + 'https://github.com/sleepymountain/AHNAppointmentChecker')
+    print('AHN Appointment Checker v1.5b | by caverin/sleepymountain')
+    print('https://github.com/sleepymountain/AHNAppointmentChecker')
     print(f'')
-    print(Fore.BLUE + '[i] Running appointment checker..')
+    print('[i] Running appointment checker..')
     print(f'')
     LOGGER.setLevel(logging.WARNING)
 
@@ -41,20 +41,20 @@ def load_page():
     with webdriver.Chrome(options=chrome_options) as driver:
         now = datetime.now()
         dt_string = now.strftime("%m/%d/%Y %I:%M %p")
-        print(Fore.BLUE + '[i] Checking for new appointments..')
+        print('[i] Checking for new appointments..')
         wait = WebDriverWait(driver, 10)
         driver.get("https://www.ahn.org/coronavirus/vaccine/schedule.html")
         if driver.find_element_by_xpath("/html/body/main/div[2]/div/section/div/div/div/div[1]/div/h1"):
-            print(Fore.RED + dt_string, " | There are no appointments available")
+            print(dt_string, " | There are no appointments available")
             logging.info(' | There are no appointments available')
             print(f'')
         else:
-            print(Fore.GREEN + "[!]", dt_string, " | There might be appointments available!")
+            print("[!]", dt_string, " | There might be appointments available!")
             logging.info(' | There might be appointments available!')
             print(f'')
             toaster.show_toast("AHN Appointment Checker", "There are appointments available!")
             os.startfile(url)
-        print(Fore.BLUE + '[i] Waiting 5 minutes.')
+        print('[i] Waiting 5 minutes.')
         time.sleep(300)
 
 
