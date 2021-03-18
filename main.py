d1c1c61b71d08a76556de8787a81225c56b237ad
sleepymountain <65647838+sleepymountain@users.noreply.github.com>
2021-03-18 09:39:10 -0400

    v1.6 Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,7 +23,7 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 def start():
 
-    print('AHN Appointment Checker v1.5b | by caverin/sleepymountain')
+    print('AHN Appointment Checker v1.6b | by caverin/sleepymountain')
     print('https://github.com/sleepymountain/AHNAppointmentChecker')
     print(f'')
     print('[i] Running appointment checker..')
@@ -37,25 +38,44 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M %p', filename='log/log.txt', level=logging.INFO)
 
 
-def load_page():
+
+def no_appt_xpath():
     with webdriver.Chrome(options=chrome_options) as driver:
+        wait = WebDriverWait(driver, 10)
+        driver.get("https://www.ahn.org/coronavirus/vaccine/schedule.html")
+        try:
+            driver.find_element_by_xpath("/html/body/main/div[2]/div/section/div/div/div/div[1]/div/h1")
+        except NoSuchElementException:
+            no_appt_xpath.no_appointments = 0
+            return False
+        else:
+            no_appt_xpath.no_appointments = 1
+            return True
+
+
+def load_page():
         now = datetime.now()
         dt_string = now.strftime("%m/%d/%Y %I:%M %p")
         print('[i] Checking for new appointments..')
-        wait = WebDriverWait(driver, 10)
-        driver.get("https://www.ahn.org/coronavirus/vaccine/schedule.html")
-        if driver.find_element_by_xpath("/html/body/main/div[2]/div/section/div/div/div/div[1]/div/h1"):
+        no_appt_xpath()
+        if no_appt_xpath.no_appointments == 1:
             print(dt_string, " | There are no appointments available")
             logging.info(' | There are no appointments available')
             print(f'')
-        else:
+        elif no_appt_xpath.no_appointments == 0:
             print("[!]", dt_string, " | There might be appointments available!")
+            print("[i] If the page loads and shows that there are no appointments, open in a private window.")
             logging.info(' | There might be appointments available!')
             print(f'')
             toaster.show_toast("AHN Appointment Checker", "There are appointments available!")
             os.startfile(url)
+        else:
+            print(dt_string, " | There are no appointments available")
+            logging.info(' | There are no appointments available')
+            print(f'')
         print('[i] Waiting 5 minutes.')
         time.sleep(300)
+
 
 
 while True:
